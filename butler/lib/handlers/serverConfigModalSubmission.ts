e6c4bcc790ac0145d026a7b0810e00441bd365d0
carassius1014 <carassius1014@gmail.com>
2021-05-21 00:58:55 +0900

    Parse ServerConfig from modal payload

--- butler/lib/handlers/serverConfigModalSubmission.ts
+++ butler/lib/handlers/serverConfigModalSubmission.ts
@@ -1,6 +1,10 @@
-import { App } from '@slack/bolt';
+import { App, Option } from '@slack/bolt';
+import { Do } from 'fp-ts-contrib/Do';
 import * as Console from 'fp-ts/Console';
+import * as E from 'fp-ts/Either';
+import { pipe } from 'fp-ts/function';
 
+import { ServerConfig } from '../domain';
 import * as ServerConfigModal from '../views/serverConfigModal';
 
 export { handle };
@@ -8,6 +12,98 @@ export { handle };
 function handle(app: App): void {
     app.view(ServerConfigModal.callbackId, async ({ ack, body }) => {
         await ack();
-        Console.log(JSON.stringify(body.view.state.values))();
+        const { values } = body.view.state;
+
+        const eServerConfig = parseServerConfig(values);
+
+        try {
+            pipe(
+                eServerConfig,
+                E.match(
+                    (err) => {
+                        throw err;
+                    },
+                    (serverConfig) => {
+                        Console.log(serverConfig)();
+                    },
+                ),
+            );
+        } catch (e) {
+            Console.error(e)();
+        }
     });
+}
+
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+interface Values {
+    [block_id: string]: {
+        [action_id: string]: any;
+    };
+}
+
+/* eslint-enable @typescript-eslint/no-explicit-any */
+
+function parseServerConfig(values: Values): E.Either<Error, ServerConfig> {
+    function buildParserError(field: string): Error {
+        return new Error(`Unable to parse ${field}`);
+    }
+
+    function parseText(block_id: string, action_id: string, field: string): E.Either<Error, string> {
+        const err = buildParserError(field);
+        const fromNullable = E.fromNullable(err);
+
+        return Do(E.Monad)
+            .bind('block', fromNullable(values[block_id]))
+            .bindL('action', ({ block }) => fromNullable(block[action_id]))
+            .bindL('value', ({ action }) => fromNullable((action as { value: string }).value))
+            .return(({ value }) => value);
+    }
+
+    function parsePlayers(block_id: string, action_id: string): E.Either<Error, string[]> {
+        const err = buildParserError('players');
+        const fromNullable = E.fromNullable(err);
+
+        return Do(E.Monad)
+            .bind('block', fromNullable(values[block_id]))
+            .bindL('action', ({ block }) => fromNullable(block[action_id]))
+            .bindL('players', ({ action }) => fromNullable((action as { selected_users: string[] }).selected_users))
+            .return(({ players }) => players);
+    }
+
+    function parseMaps(block_id: string, action_id: string): E.Either<Error, string[]> {
+        const err = buildParserError('maps');
+        const fromNullable = E.fromNullable(err);
+
+        return Do(E.Monad)
+            .bind('block', fromNullable(values[block_id]))
+            .bindL('action', ({ block }) => fromNullable(block[action_id]))
+            .bindL('options', ({ action }) => fromNullable((action as { selected_options: Option[] }).selected_options))
+            .return(({ options }) => options.map((option) => option.value as string));
+    }
+
+    const {
+        nameInputActionId,
+        nameInputBlockId,
+        passwordInputActionId,
+        passwordInputBlockId,
+        playersSelectActionId,
+        playersSelectBlockId,
+        mapsSelectActionId,
+        mapsSelectBlockId,
+    } = ServerConfigModal;
+
+    return Do(E.Monad)
+        .bind('name', parseText(nameInputBlockId, nameInputActionId, 'name'))
+        .bind('password', parseText(passwordInputBlockId, passwordInputActionId, 'password'))
+        .bind('players', parsePlayers(playersSelectBlockId, playersSelectActionId))
+        .bind('maps', parseMaps(mapsSelectBlockId, mapsSelectActionId))
+        .return(({ name, password, players, maps }) => {
+            return {
+                name,
+                password,
+                players,
+                maps,
+            };
+        });
 }
