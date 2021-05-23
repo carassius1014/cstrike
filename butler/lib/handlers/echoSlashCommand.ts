0eb477fd5e8fc348a4004c30023d64e6262380a3
carassius1014 <carassius1014@gmail.com>
2021-05-24 00:36:35 +0900

    Scaffold '/echo' command handler

---
+++ butler/lib/handlers/echoSlashCommand.ts
@@ -0,0 +1,11 @@
+import { App } from '@slack/bolt';
+import * as Console from 'fp-ts/Console';
+
+export { handle };
+
+function handle(app: App): void {
+    app.command('/echo', async ({ ack, body }) => {
+        await ack();
+        Console.log(body)();
+    });
+}
--- butler/lib/handlers/index.ts
+++ butler/lib/handlers/index.ts
@@ -1,11 +1,13 @@
 import { App } from '@slack/bolt';
 
 import * as CStrikeSlashCommand from './cstrikeSlashCommand';
+import * as EchoSlashCommand from './echoSlashCommand';
 import * as ServerConfigModalSubmission from './serverConfigModalSubmission';
 
 export { prepareHandlers };
 
 function prepareHandlers(app: App): void {
+    EchoSlashCommand.handle(app);
     CStrikeSlashCommand.handle(app);
     ServerConfigModalSubmission.handle(app);
 }
