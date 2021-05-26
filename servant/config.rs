064cc9120a3a169e5327ad76e254604abf383d88
carassius1014 <carassius1014@gmail.com>
2021-05-27 00:57:01 +0900

    [servant] Parse cstrike-related env vars when creating config

--- servant/config.rs
+++ servant/config.rs
@@ -3,15 +3,35 @@ pub mod error;
 use error::Error;
 use std::env;
 use std::net;
+use std::path;
 
+#[derive(Debug)]
 pub struct Config {
     pub address: net::SocketAddr,
+    pub path_to_maps: path::PathBuf,
+    pub path_to_mapcycle_txt: path::PathBuf,
+    pub path_to_server_cfg: path::PathBuf,
 }
 
 pub fn parse() -> Result<Config, Error> {
-    let port = env::var("SERVANT_PORT").map_err(|_| Error::FailToGetPort)?;
-    let address = format!("0.0.0.0:{}", port)
-        .parse()
-        .map_err(|_| Error::FailToParseURL)?;
-    Ok(Config { address: address })
+    fn parse_env(key: &str) -> Result<String, Error> {
+        env::var(key).map_err(|_| Error {})
+    }
+
+    fn parse_env_as_path(key: &str) -> Result<path::PathBuf, Error> {
+        parse_env(key).map(|s| path::PathBuf::from(s))
+    }
+
+    let port = parse_env("SERVANT_PORT")?;
+    let address = format!("0.0.0.0:{}", port).parse().map_err(|_| Error {})?;
+    let path_to_maps = parse_env_as_path("CSTRIKE_MAPS")?;
+    let path_to_mapcycle_txt = parse_env_as_path("CSTRIKE_MAPCYCLE_TXT")?;
+    let path_to_server_cfg = parse_env_as_path("CSTRIKE_SERVER_CFG")?;
+
+    Ok(Config {
+        address,
+        path_to_maps,
+        path_to_mapcycle_txt,
+        path_to_server_cfg,
+    })
 }
--- servant/config/error.rs
+++ servant/config/error.rs
@@ -2,18 +2,12 @@ use std::error;
 use std::fmt;
 
 #[derive(Debug)]
-pub enum Error {
-    FailToGetPort,
-    FailToParseURL,
-}
+pub struct Error {}
 
 impl error::Error for Error {}
 
 impl fmt::Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match self {
-            Error::FailToGetPort => write!(f, "Fail to get SERVANT_PORT"),
-            Error::FailToParseURL => write!(f, "Fail to parse URL"),
-        }
+        write!(f, "Fail to create config")
     }
 }
