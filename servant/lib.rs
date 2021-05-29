3e757f8034078f40f5b96ae7cef9c2081c68f2eb
carassius1014 <carassius1014@gmail.com>
2021-05-30 02:35:34 +0900

    [servant] Implement collect_maps use case

--- servant/lib.rs
+++ servant/lib.rs
@@ -1,3 +1,4 @@
 pub mod config;
 pub mod protos;
 pub mod services;
+pub mod use_cases;
---
+++ servant/use_cases/collect_maps.rs
@@ -0,0 +1,68 @@
+use crate::config::path_to_cstrike_maps;
+use crate::config::path_to_czero_maps;
+use std::collections::HashSet;
+use std::ffi::OsStr;
+use std::fs;
+use std::os::unix::ffi::OsStrExt;
+use std::path::PathBuf;
+
+pub struct Error {
+    pub message: String,
+}
+
+impl Error {
+    fn new(path: &PathBuf) -> Self {
+        let message = format!("Failed to read dir: {:?}", path);
+        Error { message }
+    }
+}
+
+pub fn run() -> Result<Vec<String>, Error> {
+    let path_to_cstrike_maps = path_to_cstrike_maps::parse().map_err(|e| Error {
+        message: e.to_string(),
+    })?;
+
+    let path_to_czero_maps = path_to_czero_maps::parse().map_err(|e| Error {
+        message: e.to_string(),
+    })?;
+
+    let cstrike_maps = collect_stems(&path_to_cstrike_maps)?;
+    let czero_maps = collect_stems(&path_to_czero_maps)?;
+
+    let mut maps: Vec<String> = cstrike_maps
+        .union(&czero_maps)
+        .into_iter()
+        .map(String::from)
+        .collect();
+
+    maps.sort();
+
+    Ok(maps)
+}
+
+fn collect_stems(path: &PathBuf) -> Result<HashSet<String>, Error> {
+    let entries = get_or_else(fs::read_dir(path), path)?;
+
+    let files: HashSet<PathBuf> = entries
+        .filter_map(Result::ok)
+        .map(|entry| entry.path())
+        .filter(|file| file.extension() == Some(OsStr::from_bytes(b"bsp")))
+        .collect();
+
+    let mut filenames = HashSet::new();
+
+    files.into_iter().for_each(|file| {
+        if let Some(stem) = file.file_stem() {
+            if let Some(filename) = stem.to_str() {
+                let filename = filename.to_string();
+                filenames.insert(filename);
+            }
+        }
+    });
+
+    Ok(filenames)
+}
+
+fn get_or_else<T, E>(value: Result<T, E>, path: &PathBuf) -> Result<T, Error> {
+    value.map_err(|_| Error::new(path))
+}
