ac65d219df542d7a8b5b2cd286aeb68dcf15fae6
carassius1014 <carassius1014@gmail.com>
2021-06-05 02:12:11 +0900

    [servant] Fix `CZERO_MAPCYCLE_TXT`

--- servant/config/path_to_map_cycle_txt.rs
+++ servant/config/path_to_map_cycle_txt.rs
@@ -3,7 +3,7 @@ use std::env;
 use std::path::PathBuf;
 
 pub fn parse() -> Result<PathBuf, Error> {
-    let key = "CSTRIKE_MAP_CYCLE_TXT";
+    let key = "CZERO_MAPCYCLE_TXT";
     env::var(key).map(|s| PathBuf::from(s)).map_err(|_| Error {
         key: key.to_string(),
     })
