3238c3d46bddceb5b991b57b0b47f874d8030a59
carassius1014 <carassius1014@gmail.com>
2021-06-05 02:11:51 +0900

    [servant] Implement regenerate_mapcycle_txt use case

--- servant/use_cases.rs
+++ servant/use_cases.rs
@@ -1,3 +1,4 @@
 pub mod collect_maps;
 pub mod error;
+pub mod regenerate_mapcycle_txt;
 pub mod regenerate_server_cfg;
---
+++ servant/use_cases/regenerate_mapcycle_txt.rs
@@ -0,0 +1,25 @@
+use super::error::Error;
+use crate::config::path_to_map_cycle_txt;
+use std::fs;
+use std::io::Write;
+use std::path;
+
+pub fn run(maps: Vec<String>) -> Result<(), Error> {
+    let path = get_or_throw(path_to_map_cycle_txt::parse())?;
+
+    let path_exists = path::Path::new(&path).exists();
+
+    if path_exists {
+        get_or_throw(fs::remove_file(&path))?
+    }
+
+    let mut file = get_or_throw(fs::File::create(path))?;
+    let maps_str = maps.join("\n");
+    get_or_throw(file.write_all(maps_str.as_bytes()))?;
+
+    Ok(())
+}
+
+fn get_or_throw<A, E: std::error::Error>(value: Result<A, E>) -> Result<A, Error> {
+    value.map_err(|e| Error::new(e.to_string().as_ref()))
+}
