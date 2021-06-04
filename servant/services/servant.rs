5c441fc975b31515d8864b9c5bad5d7c4ec8be7f
carassius1014 <carassius1014@gmail.com>
2021-06-04 12:11:18 +0900

    [servant] Handle getMaps with regenerate_server_cfg use case

--- servant/services/servant.rs
+++ servant/services/servant.rs
@@ -1,6 +1,8 @@
+use crate::domain_objects::server_config::ServerConfig;
 use crate::protos::cstrike::servant_server::{Servant, ServantServer};
 use crate::protos::cstrike::{GetMapsResponse, StartServerRequest, StartServerResponse, Unit};
 use crate::use_cases::collect_maps;
+use crate::use_cases::regenerate_server_cfg;
 use tonic::{Code, Request, Response, Status};
 
 #[derive(Debug, Default)]
@@ -9,7 +11,7 @@ pub struct Service {}
 #[tonic::async_trait]
 impl Servant for Service {
     async fn get_maps(&self, _: Request<Unit>) -> Result<Response<GetMapsResponse>, Status> {
-        let maps = get_or_else(collect_maps::run())?;
+        let maps = get_or_throw(collect_maps::run())?;
 
         let response = GetMapsResponse { maps };
 
@@ -22,9 +24,26 @@ impl Servant for Service {
     ) -> Result<Response<StartServerResponse>, Status> {
         println!("Got a request: {:?}", request);
 
-        let response = StartServerResponse {
-            success: true,
-            error_message: String::from("nothing"),
+        let StartServerRequest { name, password, players, maps: _ } = request.into_inner();
+
+        let conifg = ServerConfig {
+            hostname: name,
+            sv_password: password,
+            bot_quota: 10 - players.len() as u8,
+            ..ServerConfig::default()
+        };
+
+        let res = regenerate_server_cfg::run(&conifg);
+
+        let response = match res {
+            Ok(_) => StartServerResponse {
+                success: true,
+                error_message: String::from("nothing"),
+            },
+            Err(why) => StartServerResponse {
+                success: false,
+                error_message: why.message,
+            },
         };
 
         Ok(Response::new(response))
@@ -35,6 +54,6 @@ pub fn create() -> ServantServer<Service> {
     ServantServer::new(Service::default())
 }
 
-fn get_or_else<T, E>(value: Result<T, E>) -> Result<T, Status> {
+fn get_or_throw<T, E>(value: Result<T, E>) -> Result<T, Status> {
     value.map_err(|_| Status::new(Code::Unknown, "unknown error occurred"))
 }
