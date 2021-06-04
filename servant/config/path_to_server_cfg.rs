afb44cf8fae35ef5aef75ed9ca7fd48436bea9ef
carassius1014 <carassius1014@gmail.com>
2021-06-04 12:11:34 +0900

    [servant] Correct `CZERO_SERVER_CFG`

--- servant/config/path_to_server_cfg.rs
+++ servant/config/path_to_server_cfg.rs
@@ -3,7 +3,7 @@ use std::env;
 use std::path::PathBuf;
 
 pub fn parse() -> Result<PathBuf, Error> {
-    let key = "CSTRIKE_SERVER_CFG";
+    let key = "CZERO_SERVER_CFG";
     env::var(key).map(|s| PathBuf::from(s)).map_err(|_| Error {
         key: key.to_string(),
     })
