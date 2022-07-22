ca572dcdfb2f2857837c24a8b957d7a7a376d50e
Jiyu Zhou <carassius1014@gmail.com>
2022-07-23 00:38:59 +0900

    Add `pm2` to `shell.nix`

--- shell.nix
+++ shell.nix
@@ -5,6 +5,6 @@ let yarnFix = pkgs.yarn.override { nodejs = pkgs.nodejs-18_x; };
 in with pkgs;
 
 mkShell {
-  buildInputs = [ nodejs-18_x nixfmt yarnFix ];
+  buildInputs = [ nodejs-18_x nixfmt nodePackages.pm2 yarnFix ];
 
 }
